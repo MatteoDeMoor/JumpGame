c37b160f7f5ad56cfcf4d3ebe66c560c7326a210
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-25 12:05:53 +0100

    add level 3

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,9 +1,19 @@
 extends Node
 
-
 func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
-
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/level3.tscn")
+
+func _on_level_4_pressed():
+	pass # Replace with function body.
+
+func _on_level_5_pressed():
+	pass # Replace with function body.
+
+func _on_level_6_pressed():
+	pass # Replace with function body.
