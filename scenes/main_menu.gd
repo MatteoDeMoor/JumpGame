7ef34986b76808499476713f3ab4a8e58831a3f0
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-25 13:52:47 +0100

    level 4

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -10,7 +10,7 @@ func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://scenes/level3.tscn")
 
 func _on_level_4_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level4.tscn")
 
 func _on_level_5_pressed():
 	pass # Replace with function body.
