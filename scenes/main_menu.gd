109b8934a1bf1e5c35341921eae0bcfad2575c64
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-03-26 14:24:19 +0100

    add level 5 and 6

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,7 +13,7 @@ func _on_level_4_pressed():
 	get_tree().change_scene_to_file("res://scenes/level4.tscn")
 
 func _on_level_5_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level5.tscn")
 
 func _on_level_6_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level6.tscn")
